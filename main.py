fcd5e77bb269876b2e3c9c291ef5037e8516b69e
dwiupb1 <115912116+dwiupb1@users.noreply.github.com>
2023-01-08 00:25:14 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from view.view_nilai import * print("===================================================================" ) print("                         PROGRAM UAS SEMESTER 1                                 ") print("===================================================================" ) 
+ 
+while True:     c = input("\n(T)ambah, (U)bah, (H)apus, (C)ari, (L)ihat, (K)eluar: ") 
+ 
+    if (c.lower() == 't'):         tambah_data() 
+ 
+    elif (c.lower() == 'u'):         ubah_data() 
+ 
+    elif (c.lower() == 'h'):         hapus_data() 
+ 
+    elif (c.lower() == 'c'):         cetak_hasil_pencarian() 
+ 
+    elif (c.lower() == 'l'):         cetak_daftar_nilai() 
+ 
+    elif (c.lower() == 'k'):         break 
+ 
+    else:         print("=== Pilih Sesuai Menu Yang Tersedia ===") 
